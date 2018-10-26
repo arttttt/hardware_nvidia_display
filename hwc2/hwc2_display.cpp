Unlegacy-Android/android_device_htc_flounder

hwc2: get and set active config
Client can get and set the active config

Test: Add "TARGET_USES_HWC2 := true" to BoardConfig.mk.
      Recompile.
      Run "mm" in device/htc/flounder/hwc2.
      Push hwc2-unit-tests to device.
      Run "adb root && adb shell stop".
      Run test case. Ex: "./hwc2-unit-tests"

Change-Id: Ic506b1afb22706d71be8be2fe49056248f0bfb1e

hwc2/hwc2_display.cpp
@@ -138,6 +138,37 @@ hwc2_error_t hwc2_display::get_display_configs(uint32_t *out_num_configs,
     return HWC2_ERROR_NONE;
 }
 
+hwc2_error_t hwc2_display::get_active_config(hwc2_config_t *out_config) const
+{
+    if (!configs.size()) {
+        ALOGE("dpy %" PRIu64 ": no active config", id);
+        return HWC2_ERROR_BAD_CONFIG;
+    }
+
+    *out_config = active_config;
+
+    return HWC2_ERROR_NONE;
+}
+
+hwc2_error_t hwc2_display::set_active_config(
+        struct adf_hwc_helper *adf_helper, hwc2_config_t config)
+{
+    if (config >= configs.size()) {
+        ALOGE("dpy %" PRIu64 ": bad config", id);
+        return HWC2_ERROR_BAD_CONFIG;
+    }
+
+    int ret = adf_setMode(adf_helper, id, config);
+    if (ret < 0) {
+        ALOGE("dpy %" PRIu64 ": failed to set mode: %s", id, strerror(ret));
+        return HWC2_ERROR_BAD_CONFIG;
+    }
+
+    active_config = config;
+
+    return HWC2_ERROR_NONE;
+}
+
 hwc2_error_t hwc2_display::create_layer(hwc2_layer_t *out_layer)
 {
     hwc2_layer_t lyr_id = hwc2_layer::get_next_id();

arttttt/hardware_nvidia_display

      Run testcases: https://android-review.googlesource.com/#/q/project:
            platform/frameworks/native+branch:master+topic:test-hwc2

hwc2/hwc2_display.cpp
@@ -138,6 +138,37 @@ hwc2_error_t hwc2_display::get_display_configs(uint32_t *out_num_configs,
     return HWC2_ERROR_NONE;
 }
 
+hwc2_error_t hwc2_display::get_active_config(hwc2_config_t *out_config) const
+{
+    if (!configs.size()) {
+        ALOGE("dpy %" PRIu64 ": no active config", id);
+        return HWC2_ERROR_BAD_CONFIG;
+    }
+
+    *out_config = active_config;
+
+    return HWC2_ERROR_NONE;
+}
+
+hwc2_error_t hwc2_display::set_active_config(
+        struct adf_hwc_helper *adf_helper, hwc2_config_t config)
+{
+    if (config >= configs.size()) {
+        ALOGE("dpy %" PRIu64 ": bad config", id);
+        return HWC2_ERROR_BAD_CONFIG;
+    }
+
+    int ret = adf_set_active_config_hwc2(adf_helper, id, config);
+    if (ret < 0) {
+        ALOGE("dpy %" PRIu64 ": failed to set mode: %s", id, strerror(ret));
+        return HWC2_ERROR_BAD_CONFIG;
+    }
+
+    active_config = config;
+
+    return HWC2_ERROR_NONE;
+}
+
 hwc2_error_t hwc2_display::create_layer(hwc2_layer_t *out_layer)
 {
     hwc2_layer_t lyr_id = hwc2_layer::get_next_id();